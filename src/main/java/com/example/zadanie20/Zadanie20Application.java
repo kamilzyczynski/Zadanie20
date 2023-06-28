8c6f02ed480961a7c9f2494610742d84f57be25d
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-06-28 22:34:50 +0200

    Initial commit

---
+++ src/main/java/com/example/zadanie20/Zadanie20Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie20/Zadanie20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
