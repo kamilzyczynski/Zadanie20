b96ea18f2e3365443c147d74873238e980f0cb3c
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-06-30 16:05:35 +0200

    Poprawione rozwiązanie.

--- src/main/java/com/example/zadanie20/UsersController.java
+++ src/main/java/com/example/zadanie20/UsersController.java
@@ -23,14 +23,15 @@ public class UsersController {
         }
         return result;
     }
+
     @RequestMapping("add")
     String addUser(@RequestParam(value = "imie", required = false) String firstName, @RequestParam(value = "nazwisko", required = false) String lastName,
-    @RequestParam(value = "wiek", required = false, defaultValue = "0") Integer age) {
-    if (!firstName.isEmpty() && !lastName.isEmpty() && age != null) {
-        userRepository.addUser(firstName, lastName, age);
-        return "redirect:/success.html";
-    } else {
-        return "redirect:/err.html";
-    }
+                   @RequestParam(value = "wiek", required = false, defaultValue = "0") Integer age) {
+        if (!firstName.isEmpty() && !lastName.isEmpty() && age != null) {
+            userRepository.addUser(firstName, lastName, age);
+            return "redirect:/success.html";
+        } else {
+            return "redirect:/err.html";
+        }
     }
 }
